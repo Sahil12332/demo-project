6fc2420642133daf02f085d147d0ef8b2675bbcc
Arvind.Rajput <arvind.rajput@synechron.com>
2022-12-27 13:54:32 +0530

    fix

--- src/dataset.js
+++ src/dataset.js
@@ -6,7 +6,7 @@ const dataset = {
     "task-4": { id: "task-4", content: "Content for task-4" }
   },
   columns: {
-    "column-1": { id: "column-1", title: "User", taskIds: ['task-1'] },
+    "column-1": { id: "column-1", title: "Users", taskIds: ['task-1'] },
     "column-2": { id: "column-2", title: "In progress", taskIds: ['task-2', 'task-3'] },
     "column-3": { id: "column-3", title: "Review", taskIds: [] },
     "column-4": { id: "column-4", title: "Completed", taskIds: ["task-4"] }
